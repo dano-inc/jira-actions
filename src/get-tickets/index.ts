72dd479622c96e59e9bf36cc07d21ec48337dfae
이찬희 <iamchanii.0310@gmail.com>
2021-12-08 18:34:11 +0900

    log

--- src/get-tickets/index.ts
+++ src/get-tickets/index.ts
@@ -1,15 +1,17 @@
-import * as github from '@actions/github';
-import * as core from '@actions/core';
+import * as github from "@actions/github";
+import * as core from "@actions/core";
 
 async function main() {
-    const repoToken = core.getInput('repo-token');
+  const repoToken = core.getInput("repo-token");
 
-    const commits = await github.getOctokit(repoToken).rest.repos.listCommits({
-        owner: github.context.payload.repository!.owner.login,
-        repo: github.context.payload.repository!.name,
-    });
+  const commits = await github.getOctokit(repoToken).rest.repos.listCommits({
+    owner: github.context.payload.repository!.owner.login,
+    repo: github.context.payload.repository!.name,
+  });
 
-    console.log(commits);
+  commits.data.forEach((commit) => {
+    console.log(commit.commit.message);
+  });
 }
 
 main();
