49424b38d194fcc22c68d9e56f544712778a1fac
이찬희 <iamchanii.0310@gmail.com>
2021-12-08 18:28:12 +0900

    add github-token input

--- src/get-tickets/index.ts
+++ src/get-tickets/index.ts
@@ -2,7 +2,7 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 
 async function main() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const GITHUB_TOKEN = core.getInput('github-token');
 
     const commits = await github.getOctokit(GITHUB_TOKEN).rest.repos.listCommits({
         owner: github.context.payload.repository!.owner.login,
