ac2d0fb2f838e9dd8d5f2f56c6d3705ba65c7b06
이찬희 <iamchanii.0310@gmail.com>
2021-12-08 18:31:01 +0900

    use repo token

--- src/get-tickets/index.ts
+++ src/get-tickets/index.ts
@@ -2,9 +2,9 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 
 async function main() {
-    const GITHUB_TOKEN = core.getInput('github-token');
+    const repoToken = core.getInput('repo-token');
 
-    const commits = await github.getOctokit(GITHUB_TOKEN).rest.repos.listCommits({
+    const commits = await github.getOctokit(repoToken).rest.repos.listCommits({
         owner: github.context.payload.repository!.owner.login,
         repo: github.context.payload.repository!.name,
     });
